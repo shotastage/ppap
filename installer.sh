807aae1b78294db6f2b3a0a2000b154bbc78056a
Shota Shimazu <shotastage@users.noreply.github.com>
2016-10-23 23:37:04 +0900

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
 
-mkdir .install_tmp
-cd .install_tmp
-git clone https://github.com/shotastage/ppap.git
+
+install_main() {
+  mkdir .install_tmp
+  cd .install_tmp
+  git clone https://github.com/shotastage/ppap.git $HOME/.Toolchains/papp/
+  cd ..
+}
+
+add_path() {
+  echo 'export PATH=$HOME/.Toolchains/papp:"${PATH}"' >> $HOME/.bash_profile
+}
+
+
+install_main
+add_path
+exec bash -l
